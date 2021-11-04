ab1241ed022511fe2201dcabc8e84b111be7fa5d
Andrew Turner <turner.andrew0@gmail.com>
2021-11-04 11:09:35 -0500

    rollbar update

--- public/server.js
+++ public/server.js
@@ -3,6 +3,7 @@ const app = express()
 const path = require('path')
 
 var Rollbar = require("rollbar");
+const { rootCertificates } = require('tls');
 var rollbar = new Rollbar({
   accessToken: 'dd1d04d4f7624e2294ef55a2726312b0',
   captureUncaught: true,
@@ -36,10 +37,13 @@ app.post('/api/students', (req, res) => {
     try{
         if (index === -1 && name !== ''){
             students.push(name)
+            rollbar.info('Someone added a student.')
             res.status(200).send(students)
         } else if (name === '') {
+            rollbar.error('Someone tried to enter a blank student')
             res.status(400).send('must enter a student name')
         } else {
+            rollbar.error('Someone tried to enter a duplicate student')
             res.status(400).send('that student already exists')
         }
     } catch (err) {
